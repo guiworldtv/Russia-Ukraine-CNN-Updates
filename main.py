a5edb9e008909b4fd9f6fc7d5ad5058e96a23996
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 10:59:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pprint import pprint as pp
 from bs4 import BeautifulSoup
 
-base_url = 'https://cnnportugal.iol.pt/-{}-{}-{}{}/index.html'
+base_url = 'https://www.cnnbrasil.com.br-{}-{}-{}{}/index.html'
             
 
 tz_WA = pytz.timezone('US/Pacific')
