36cebb57a07118cb6484f445263190edc0e1c5c8
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-07-29 10:56:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pprint import pprint as pp
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.r7.com-{}-{}-{}{}/index.html'
+base_url = 'https://g1.globo.com-{}-{}-{}{}/index.html'
             
 
 tz_WA = pytz.timezone('US/Pacific')
